d3aefd94e7a30474246478a0a2975f963c60252b
Rigene Salang <rigene.salang@gmail.com>
2023-03-31 18:26:48 +1100

    desktop layout with some style

---
+++ script.js
@@ -0,0 +1,32 @@
+// DESKTOP TIME
+
+const currentTime = () => {
+	const date = new Date();
+	let hh = date.getHours();
+	let mm = date.getMinutes();
+	// let ss = date.getSeconds();
+	let ampm = "AM";
+
+	// settin if AM or PM
+	if (hh > 12) {
+		hh -= 12;
+		ampm = "PM";
+	}
+
+	//adding 0 before single digit times
+	mm = mm < 10 ? "0" + mm : mm;
+	// ss = ss < 10 ? "0" + ss : ss;
+
+	const clock = `${hh}:${mm} ${ampm}`;
+
+	document.getElementById("desktopTime").innerHTML = clock;
+	setTimeout(() => {
+		currentTime();
+	}, 1000);
+};
+
+currentTime();
+
+// Start Button opening
+
+//
