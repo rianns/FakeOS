d06e934057cb22a5f7e7b7ef432cbbf2de08262d
Rigene Salang <rigene.salang@gmail.com>
2023-04-03 01:11:03 +1000

    windows behaviour done

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Start Button opening
 // toggle startMenu div at startButton press
 
+const startButtonSection = document.getElementById("startMenu");
+
 const startButtonToggle = () => {
-	const startButtonSection = document.getElementById("startMenu");
 	if (startButtonSection.style.display === "none") {
 		startButtonSection.style.display = "block";
 	} else {
@@ -13,6 +14,77 @@ const startButtonToggle = () => {
 document
 	.getElementById("startButton")
 	.addEventListener("click", startButtonToggle, false);
+
+// Start Menu closing after an application is opened
+const closeAfterAppOpen = (windowName) => {
+	const appDiv = document.querySelector(windowName);
+	const appDivComp = window.getComputedStyle(appDiv);
+	if (appDivComp.display === "none") {
+		appDiv.style.display = "block";
+		startButtonSection.style.display = "none";
+	}
+};
+
+// Closing applications with red button on top right of window
+const closeApp = (openWin) => {
+	const appDiv = document.querySelector(openWin);
+	const appDivComp = window.getComputedStyle(appDiv);
+	if (appDivComp.display !== "none") {
+		appDiv.style.display = "none";
+	}
+};
+
+// Draggable windows
+// selecting windows/area to drag
+const dragWindows = (e) => {
+	// set initial pos
+	let currX = 0,
+		currY = 0,
+		nextX = 0,
+		nextY = 0;
+
+	// initial window position
+	e.querySelector(".window-open").onmousedown = (el) => {
+		el.preventDefault();
+		nextX = el.clientX;
+		nextY = el.clientY;
+
+		document.onmouseup = atMoveStop;
+		document.onmousemove = atMouseMove;
+	};
+
+	// window movement
+	const atMouseMove = (el) => {
+		el.preventDefault();
+
+		currX = nextX - el.clientX;
+		currY = nextY - el.clientY;
+
+		nextX = el.clientX;
+		nextY = el.clientY;
+
+		e.style.top = e.offsetTop - currY + "px";
+		e.style.left = e.offsetLeft - currX + "px";
+	};
+
+	// at movement end
+	const atMoveStop = () => {
+		document.onmouseup = null;
+		document.onmousemove = null;
+	};
+};
+
+// Calculator
+
+dragWindows(document.querySelector(".calculator"));
+
+// Notepad
+
+dragWindows(document.querySelector(".notepad"));
+
+// Video Player
+
+dragWindows(document.querySelector(".video-player"));
 
 // DESKTOP TIME
 
